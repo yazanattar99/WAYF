4570e37d63663dced3912f8521fbc1737f6f45a5
Yazan Al Attar <yazan@Yazans-MacBook-Pro-2.local>
2022-03-22 15:23:14 -0700

    First iteration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Typewriter from "typewriter-effect";
+
+const bounceButtonStyle = {
+  backgroundColor: "grey",
+  color: "white",
+  margin: "100px 2px",
+};
+function BounceButton(props) {
+  return (
+    <button
+      style={bounceButtonStyle}
+      onClick={(e) => {
+        e.preventDefault();
+        window.location.href =
+          "http://www.bouncelife.com/events/622024c94c917e0e2fa83090";
+      }}
+    >
+      Get Tickets!
+    </button>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Riddler_div">
+      <Typewriter
+        options={{
+          strings: [
+            "Looking for Friday night plans?",
+            "Did you miss the last WAYF event?",
+            "Are you ready for the biggest Reunion of 2022?",
+            "03-25-2022",
+          ],
+          autoStart: true,
+          loop: true,
+          onRemoveNode: {},
+        }}
+        // onInit={(typewriter) => {
+        //   typewriter
+        //     .typeString("Hello World!")
+        //     .callFunction(() => {
+        //       console.log("String typed out!");
+        //     })
+        //     .pauseFor(2500)
+        //     .deleteAll()
+        //     .callFunction(() => {
+        //       console.log("All strings were deleted");
+        //     })
+        //     .start();
+        // }}
+      />
+      <div>
+        <BounceButton />
+      </div>
     </div>
   );
 }
